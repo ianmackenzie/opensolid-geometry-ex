1d9d3946a91724bcd32b5ef3601d0a995843c4d9
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-01-10 14:18:27 -0500

    Add package metadata

--- mix.exs
+++ mix.exs
@@ -4,6 +4,11 @@ defmodule OpenSolid.Geometry.Mixfile do
   def project do
     [app: :opensolid_geometry,
      version: "0.1.0",
+     description: "Elixir implementation of opensolid/geometry",
+     package:
+      [licenses: ["MPL-2.0"],
+       maintainers: ["Ian Mackenzie <ian.e.mackenzie@gmail.com>"],
+       links: %{"GitHub" => "https://github.com/ianmackenzie/opensolid-geometry-ex"}],
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
