d5c0b180e8666909b9a2698f5ade39e22fdcf164
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-01-12 18:08:48 -0500

    Add Point2d.from_coordinates_in_frame
    Experimenting with functions that work well with and without pipelining

--- lib/opensolid/point2d.ex
+++ lib/opensolid/point2d.ex
@@ -24,6 +24,21 @@ defmodule OpenSolid.Point2d do
     coordinates_
   end
 
+  @spec from_coordinates_in_frame(number, number, term) :: t
+  def from_coordinates_in_frame(x, y, frame) do
+    {:frame2d, origin_point, x_direction, y_direction} = frame
+    {x0, y0} = coordinates(origin_point)
+    {:direction2d, {x1, y1}} = x_direction
+    {:direction2d, {x2, y2}} = y_direction
+    {:point2d, {x0 + x * x1 + y * x2, y0 + x * y1 + y * y2}}
+  end
+
+  @spec from_coordinates_in_frame({number, number}, term) :: t
+  def from_coordinates_in_frame(coordinates, frame) do
+    {x, y} = coordinates
+    from_coordinates_in_frame(x, y, frame)
+  end
+
   @spec vector_to(t, t) :: Vector2d.t
   def vector_to(first_point, second_point) do
     {x1, y1} = coordinates(first_point)
